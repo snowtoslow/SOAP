1f0afdb92a04a7a56f3743f0a1572a34fa8d3251
snowtoslow <snowtoslow@gmail.com>
2020-03-03 11:28:35 +0200

    add all necessary dependencies for soap project

---
+++ src/test/java/com/soap/SoapWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
